9992c5706672882e3643230d9bf560c6084a3645
EvelisaFlores <evelisa.flores@gmail.com>
2020-06-23 14:37:23 -0400

    In progress

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,17 +4,18 @@ import csv
 
 #Path for file
 csvpath = os.path.join('..','PyPoll','Resources','election_data.csv')
-#textpath =os.path.join('..','PyPoll','Analysis','election_results.txt')  
+textpath =os.path.join('..','PyPoll','Analysis','election_results.txt')  
 
+#Counting votes
+total_votes = 0
 
-count_votes = 0
-#candidates = 0
+#candidates list and candidates votes
 candidates_list = []
 candidate_votes = {}
-votes_percentage = 0
-votes = 0
-winner = ""
-winning_votes = 0
+
+#Winner
+winner_candidate = ""
+winner_count = 0
 
 #Reading CSV file
 with open(csvpath) as csvfile:
@@ -24,41 +25,53 @@ with open(csvpath) as csvfile:
     #Read each row of data after the header
     for row in csvreader:
        #Counting number of votes
-         count_votes = count_votes + 1
+         total_votes = total_votes + 1
        #Candidates list
          candidates_name = row[2]
     #The total number of votes each candidate won
          if (candidates_name not in candidates_list):
              candidates_list.append(candidates_name)
              candidate_votes[candidates_name]= 0
-         else:
-             candidate_votes[candidates_name]=candidate_votes[candidates_name] + 1
-   
-         for candidates_name in candidate_votes:
-             votes = candidates_votes.get(candidates_name)
-             votes_percentage = float(votes/float(count_votes)*100
-    #winner
-             if (votes > winning_votes):
-                 
- 
+            
+         candidate_votes[candidates_name]=candidate_votes[candidates_name] + 1
 
+    #Print Results 
+    print("\n")
+    print(f"Election Results\n")
+    print(f"----------------------------\n")
+    print(f"Total Votes: {total_votes}\n")
+    print(f"----------------------------\n") 
+    #Create text file
+    with open(textpath, 'w') as txtfile:
+         txtfile.write(f"Election Results\n")
+         txtfile.write(f"----------------------------\n")
+         txtfile.write(f"Total Votes: {total_votes}\n")
+         txtfile.write(f"----------------------------\n")
 
-
-   # for candidates in candidates_with_votes_list:
-      #  percentage_votes_won = (candidate_votes[candidates]/votes)*100
-       # print(candidates_votes)
-
-    print("Election Results\n")
-    print("-------------------------\n")
-    print(f"Total Votes: {votes}\n")
-    print("-------------------------\n")
-    print(percentage_votes_won)
+    #Percentages
+    for candidate in candidate_votes:
+         votes = candidate_votes.get(candidate)
+         vote_percentage = float(votes)/float(total_votes) * 100
+         result = f"{candidate}:{vote_percentage:.3f}% ({votes})\n"
+         print(result)
+         with open(textpath, 'a') as txtfile:
+                txtfile.write(result)
+         
+         
+         if(votes> winner_count):
+           winner_count = votes
+           winner_candidate = candidate
+           winner_result = f"Winner:{winner_candidate}\n"
+    with open(textpath, 'a') as txtfile:
+           txtfile.write(f"----------------------------\n")
+           txtfile.write(winner_result)
+           txtfile.write(f"----------------------------\n")
+              
+    #print(result)
+    print(f"----------------------------\n") 
+    print(winner_result)
+    print(f"----------------------------\n") 
     
-            
-            
-       
-        
 
-       
 
-        
+    
